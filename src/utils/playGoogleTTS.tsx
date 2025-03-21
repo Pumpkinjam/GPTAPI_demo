31f4a5ff3859aba69f756e0ccff0063427002d00
Pumpkinjam <rnjsl1212@naver.com>
2025-03-21 17:55:47 +0900

    fixed problem: Google TTS reading newLine character

--- src/utils/playGoogleTTS.tsx
+++ src/utils/playGoogleTTS.tsx
@@ -1,14 +1,15 @@
 // Google TTS 호출 함수
 
 export async function playGoogleTTS(text: string) {
-  
+    const replacedText = text.split('\n').join(', '); // TTS가 줄바꿈 문자를 그대로 읽어버리는 문제 수정
+
     const response = await fetch(`https://texttospeech.googleapis.com/v1/text:synthesize?key=${import.meta.env.VITE_GCloud_API_KEY}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify({
-        input: { text },
+        input: { text: replacedText },
         voice: { languageCode: 'ko-KR', name: 'ko-KR-Chirp3-HD-Kore' }, //Aoede, Kore, Leda, Orus, Puck 등등
         audioConfig: { audioEncoding: 'MP3' },
       }),
